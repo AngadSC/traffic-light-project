7186e885deebf578481901f08056c182978e7e3b
AngadSC <104170661+AngadSC@users.noreply.github.com>
2022-05-11 14:57:03 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+input.onButtonPressed(Button.A, function () {
+    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        basic.showLeds(`
+            # . . . #
+            # . # . #
+            # . # . #
+            # # # # #
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # . . . .
+            # # # # #
+            . . . . #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(4000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(5000)
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . .
+            # # # # #
+            . . . . #
+            # # # # #
+            `)
+    }
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . .
+            # # # # #
+            . . . . #
+            # # # # #
+            `)
+    }
+})
