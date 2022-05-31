078caa59f4a0bd4afd6f6da2f608082314813920
AngadSC <104170661+AngadSC@users.noreply.github.com>
2022-05-30 21:04:22 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,8 +18,8 @@ basic.forever(function () {
     pins.analogWritePin(AnalogPin.P1, 1023)
     pins.analogWritePin(AnalogPin.P2, 0)
     basic.pause(1000)
-    pins.analogWritePin(AnalogPin.P0, 1023)
-    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 1023)
     pins.analogWritePin(AnalogPin.P2, 1023)
 })
 basic.forever(function () {
